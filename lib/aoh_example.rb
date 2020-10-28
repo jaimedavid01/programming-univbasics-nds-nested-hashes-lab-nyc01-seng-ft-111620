441ce395153fa4fb2eb30af5b1dd01efa0d1ba2a
Jaime David <jaimedavid01@yahoo.com>
2020-10-28 00:13:04 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -26,6 +26,7 @@ def literal_aoh
   array_literal = [{ name:  "Don Gately", occupation: "Live-in Staff" },
   { name:  "Joelle van Dyne", occupation: "Radio Personality" },
   { name:  "Pat Monteseian", occupation: "Staff" },
+  { name:  "Kate Gompert", occupation: "None" },
   
   ]
   
